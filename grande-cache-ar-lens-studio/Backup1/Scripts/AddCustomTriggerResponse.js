5fd0a1f490b9b84d2af271ca3c0f20aea3a3c67a
Alex Mayer <alexander.mayer@lethbridgecollege.ca>
2021-06-28 12:52:28 -0600

    Creating Backup
    Large Push

--- grande-cache-ar-lens-studio/Backup1/Scripts/AddCustomTriggerResponse.js
+++
@@ -1,8 +0,0 @@
-// @input string triggerName
-// @output execution onTrigger
-
-if (!global.behaviorSystem) {
-    print("Behavior system not initialized. Make sure a Behavior script is present in the scene, and above this in the Objects panel hierarchy.");
-}
-
-global.behaviorSystem.addCustomTriggerResponse(script.triggerName, script.onTrigger);
